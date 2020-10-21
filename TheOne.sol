d3f37ca8c06f27acec94b8767bd675e5ca17f3a1
TheophileBonifaceAchille <theophile.boniface@gmail.com>
2020-10-21 12:56:15 +0200

    first commit

---
+++ TheOne.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT                 // SPDX Licence Identifier
+pragma solidity ^0.6.0; // Version pragma
+pragma experimental ABIEncoderV2; // Experimental pragma
+
+contract TheOne {
+    function one(uint256 x) public pure returns (uint256) {
+        return x = 1;
+    }
+}
