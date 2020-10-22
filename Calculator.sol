30a62fdf7da872ded3952a4c2a2822af7a57b18e
TheophileBonifaceAchille <theophile.boniface@gmail.com>
2020-10-22 09:50:24 +0200

    calc finish

--- Calculator.sol
+++ Calculator.sol
@@ -2,30 +2,42 @@
 pragma solidity ^0.6.0; // Version pragma
 pragma experimental ABIEncoderV2; // Experimental pragma
 
-import './Adder.sol';
-import './Suber.sol';
-import './Multiplier.sol';
-import './Divisor.sol';
+import "./Adder.sol";
+import "./Suber.sol";
+import "./Multiplier.sol";
+import "./Divisor.sol";
 
 contract Calculator {
-    Adder public adder;
-    Suber public suber;
-    Multiplier public multiplier;
-    Divisor public divisor;
-    
-    constructor(address _adder, address _suber, address _multiplier, address _divisor) public {
+    Adder private adder;
+    Suber private suber;
+    Multiplier private multiplier;
+    Divisor private divisor;
+
+    constructor(
+        address _adder,
+        address _suber,
+        address _multiplier,
+        address _divisor
+    ) public {
         adder = Adder(_adder);
         suber = Suber(_suber);
         multiplier = Multiplier(_multiplier);
         divisor = Divisor(_divisor);
     }
 
-    // Pas fini
-    
-    function calc(uint nb1, uint nb2) public pure returns(uint) {
-        return adder(nb1, nb2);
-        return suber(nb1, nb2);
-        return multiplier(nb1, nb2);
-        return divisor(nb1, nb2);
+    function calcAdd(uint256 nb1, uint256 nb2) public view returns (uint256) {
+        return adder.adder(nb1, nb2);
     }
-}   
+
+    function calcSub(uint256 nb1, uint256 nb2) public view returns (uint256) {
+        return suber.suber(nb1, nb2);
+    }
+
+    function calcMul(uint256 nb1, uint256 nb2) public view returns (uint256) {
+        return multiplier.multiplier(nb1, nb2);
+    }
+
+    function calcDiv(uint256 nb1, uint256 nb2) public view returns (uint256) {
+        return divisor.divisor(nb1, nb2);
+    }
+}
